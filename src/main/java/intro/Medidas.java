62b7ac24483cc7220481f3c973465dd6a0085659
RodrigoFructuoso1984 <fructuoso.digao@gmail.com>
2021-06-06 20:53:04 -0300

    Criação do primeiro código em JAVA.

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,27 @@
+// 1 - Pacote
+package intro;
+
+// 2 - Referência bibliotecas
+
+// 3 - Classe
+public class Medidas {
+    // 3.1 - Atributos - Características
+
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args){
+        System.out.println("Meu primeiro codigo em JAVA !!!");
+        System.out.println("Intervalo: Voltaremos 12:25");
+
+        // Calculo da area - Exemplo: o tamanho do tapete ou do piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 5; // largura recebe 4
+        comprimento = 6; // comprimento recebe 3
+        resultado = largura * comprimento;
+
+        System.out.println("A area é de " + resultado);
+    }
+
+}
